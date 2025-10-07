bfcf0730945719e26f1e7687537947607c895987
Alex Buckley <alexmeb81@outlook.com>
2025-10-07 00:03:39 -0700

    Implemented basic structure for test run

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,9 @@
 from pathlib import Path
 import yaml
 from typing import Any
+import tempfile
+
+TEST_FILE_NAME = "test.sml"
 
 def load_tests() -> list[dict[str, any]]:
     '''
@@ -15,7 +18,32 @@ def load_tests() -> list[dict[str, any]]:
     with open(yaml_path, 'r') as file:
         return yaml.safe_load(file) # Return data as list
 
-def run_test():
-    test_data: list[dict[str, any]] = load_tests();
+def create_sml_file() -> Path:
+    '''
+    Create temp directory and sml file
+    '''
+    temp_dir = Path(tempfile.mkdtemp())
+    return temp_dir / TEST_FILE_NAME    
+
+def test_function(test_func: dict[str, any], test_file: Path) -> None:
+    '''
+    Conduct test for sml function argument
+    '''
+    func_name: str = test_func["name"]
+    cases: list[dict[str, str]] = test_func["cases"]
+
+    for case in cases:
+        write_test_file()
+        test_case()
+
+
+
+def run_test() -> None:
+    test_data = load_tests()
+    test_file = create_sml_file()
+
+    for func in test_data:
+        test_function(func, test_file)
+        
 
     
