542c65697d971cf82ebd4fdc55de85d06ed31f9a
Alex Buckley <alexmeb81@outlook.com>
2025-10-07 03:29:04 -0700

    Implemented Test logic

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,8 +2,38 @@ from pathlib import Path
 import yaml
 from typing import Any
 import tempfile
+import subprocess
+import shutil
+import pytest
 
-TEST_FILE_NAME = "test.sml"
+
+TEST_FILE_NAME: str = "test.sml"
+
+
+@pytest.fixture(scope="session")
+def target_file(request):
+    target_name = request.config.getoption("--target")
+    repo_root = Path(__file__).parent.parent.resolve()
+    return repo_root / "src" / "individual_work" / target_name
+
+
+@pytest.fixture(scope="session")
+def test_file():
+    # Create test sml file in temp directory
+    temp_dir = Path(tempfile.mkdtemp())
+    test_file = temp_dir / TEST_FILE_NAME
+
+    # Run tests then delete directory
+    yield test_file
+    shutil.rmtree(temp_dir)
+
+
+def create_test_params(func: dict[str, any]) -> list[tuple[str, str, str]]:
+    '''
+    Zip test case data in tlist of tuples for parametrize
+    '''
+    return [(func["name"], case["args"], case["expected"]) for case in func["cases"]]
+
 
 def load_tests() -> list[dict[str, any]]:
     '''
@@ -17,33 +47,31 @@ def load_tests() -> list[dict[str, any]]:
     # Load yaml file
     with open(yaml_path, 'r') as file:
         return yaml.safe_load(file) # Return data as list
-
-def create_sml_file() -> Path:
-    '''
-    Create temp directory and sml file
-    '''
-    temp_dir = Path(tempfile.mkdtemp())
-    return temp_dir / TEST_FILE_NAME    
-
-def test_function(test_func: dict[str, any], test_file: Path) -> None:
-    '''
-    Conduct test for sml function argument
-    '''
-    func_name: str = test_func["name"]
-    cases: list[dict[str, str]] = test_func["cases"]
-
-    for case in cases:
-        write_test_file()
-        test_case()
-
-
-
-def run_test() -> None:
-    test_data = load_tests()
-    test_file = create_sml_file()
-
-    for func in test_data:
-        test_function(func, test_file)
-        
-
     
+
+def generate_test_cases() -> list[tuple[str, str, str]]:
+    '''
+    Generate list of tuples for all test cases in tests.yaml
+    '''
+    return [case for func in load_tests() for case in create_test_params(func)]
+
+
+def write_test_file(test_file: Path, name: str, args: str, target_file: Path) -> None:
+    func_call = f"use {str(target_file)};\n{name}{args};"
+    test_file.write_text(func_call, encoding='utf-8')
+
+@pytest.mark.parametrize("func, args, expected", generate_test_cases())
+def run_test(func: str, args: str, expected: str, test_file: Path, target_file: Path):
+    write_test_file(test_file, func, args, target_file)
+    result = subprocess.run(
+        ["sml", str(test_file)],
+        capture_output=True,
+        text=True
+    )
+
+    assert result.stdout.strip() == expected.strip()
+
+
+def pytest_addoption(parser):
+    parser.addoption("--target", action="store", help="Target SML file name")
+
